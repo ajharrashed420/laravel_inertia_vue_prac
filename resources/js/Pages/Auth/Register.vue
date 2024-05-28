068c7fdb4b3bf80b6f0b73ecbb14abdb903518ff
Ajharul Islam <ajharrashed420@gmail.com>
2024-05-28 20:36:51 +0600

    7# Text Input Component for Reuse | Register User (Part-4)

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -5,29 +5,33 @@
 
     <div class="w-2/4 mx-auto">
         <form @submit.prevent="submit">
-            <div class="mb-6">
-                <label for="name">Name</label>
-                <input type="text" v-model="form.name">
-                <small>{{ form.errors.name }}</small>
-            </div>
 
-            <div class="mb-6">
-                <label for="email">Email</label>
-                <input type="email" v-model="form.email">
-                <small>{{ form.errors.email }}</small>
-            </div>
+            <TextInput
+             name="name" 
+             v-model="form.name" 
+             :message="form.errors.name" 
+             />
+             
+            <TextInput
+             name="email" 
+             type="email" 
+             v-model="form.email" 
+             :message="form.errors.email" 
+             />
 
-            <div class="mb-6">
-                <label for="name">Password</label>
-                <input type="password" v-model="form.password">
-                <small>{{ form.errors.password }}</small>
-            </div>
-
-            <div class="mb-6">
-                <label for="name">Confirm Password</label>
-                <input type="password" v-model="form.password_confirmation">
-            </div>
-
+            <TextInput
+             name="password" 
+             type="password" 
+             v-model="form.password" 
+             :message="form.errors.password" 
+             />
+            
+            <TextInput
+             name="confirm password" 
+             type="password" 
+             v-model="form.password_confirmation"
+            />
+            
             <div>
                 <p>Already a user? <a class="text-link" href="#">Login</a></p>
                 <button class="btn w-full my-2 font-bold" :disabled="form.processing">Register</button>
@@ -40,8 +44,8 @@
 </template>
 
 <script setup lang="ts">
-    import { reactive } from 'vue'
-import { useForm } from '@inertiajs/vue3'
+import { useForm } from '@inertiajs/vue3';
+import TextInput from "../Components/TextInput.vue";
 
 const form = useForm({
         name: null,
---
+++ resources/js/Pages/Components/TextInput.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="mb-6">
+        <label for="name">{{ name }}</label>
+        <input :type="type" v-model="model" :class="{'!ring-red-500' : message}">
+        <small class="error" v-if="message">{{ message }}</small>
+    </div>
+</template>
+
+<script setup lang="ts">
+    const model = defineModel({
+        type: null,
+        required: true,
+    });
+
+    defineProps({
+        name: {
+            type: String,
+            required: true
+        },
+        type: {
+            type: String,
+            default: 'text',
+        },
+        message: String,
+    });
+
+    
+</script>
+
+<style scoped>
+
+</style>
