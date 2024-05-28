9248eeaee6cb6e87e5d5786ba980d71bdd7a9b6e
Ajharul Islam <ajharrashed420@gmail.com>
2024-05-28 12:07:39 +0600

    7# Get Register Form Data (Part-2)

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -4,25 +4,25 @@
     <h1 class="text-center text-3xl font-bold text-gray-600 uppercase py-2">Register a new account</h1>
 
     <div class="w-2/4 mx-auto">
-        <form action="">
+        <form @submit.prevent="submit">
             <div class="mb-6">
                 <label for="name">Name</label>
-                <input type="text">
+                <input type="text" v-model="form.name">
             </div>
 
             <div class="mb-6">
                 <label for="email">Email</label>
-                <input type="email">
+                <input type="email" v-model="form.email">
             </div>
 
             <div class="mb-6">
                 <label for="name">Password</label>
-                <input type="password">
+                <input type="password" v-model="form.password">
             </div>
 
             <div class="mb-6">
                 <label for="name">Confirm Password</label>
-                <input type="password">
+                <input type="password" v-model="form.password_confirmation">
             </div>
 
             <div>
@@ -37,7 +37,18 @@
 </template>
 
 <script setup lang="ts">
+    import { reactive } from 'vue'
 
+    const form = reactive({
+        name: null,
+        email: null,
+        password: null,
+        password_confirmation: null,
+    });
+
+    const submit = () => {
+        console.log(form);
+    }
 </script>
 
 <style scoped>
