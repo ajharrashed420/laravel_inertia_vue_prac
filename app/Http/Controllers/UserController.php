faff0527c417b58875e9a0f091c74a31561a51f8
Ajharul Islam <ajharrashed420@gmail.com>
2024-05-30 16:56:11 +0600

    Fix some issue

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,29 +3,35 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Inertia\Inertia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
    public function index(Request $request) {
-        return inertia('Home', 
-            ['users' => User::when($request->search, function($query) use ($request) {
+    
+    $users = User::when($request->search, function($query) use ($request) {
                 $query
                 ->where('name', 'like', '%' . $request->search . '%')
                 ->orWhere('email', 'like', '%' . $request->search . '%')
                 ;
-            })->paginate(8)->withQueryString(),
+            })->paginate(8)->withQueryString();
 
-            'searchTerm' => $request->search,
+    return Inertia::render('Home', [
+        'users' => $users,
 
-            'can' => [
-                'delete_user' => 
-                    Auth::user() ? 
-                    Auth::user()->can('delete', User::class) : 
-                    null
-            ]
-        ]);
+        'searchTerm' => $request->search,
+        //Give permission to show delete user section / not delete
+        'can' => [
+            'delete_user' => 
+                Auth::user() ? 
+                Auth::user()->can('delete', User::class) : 
+                null
+        ]
+
+    ]);
+
     }
 
 
